9eecd9eb3308a53ae5dd8e4e4207df900e94519e
Cindy Zhang <cixzhang@gmail.com>
2016-08-23 08:42:10 -0700

    rendering a line as a stem

--- src/create-image-data.js
+++ src/create-image-data.js
@@ -10,9 +10,20 @@ export default function createImageData(plant) {
 
 function generateStemPixels(plant, width, height) {
   var pixels = new Uint8ClampedArray(4 * width * height);
-  drawPixel(pixels, width, 0, 0, [255, 0, 0, 255]);
-  drawPixel(pixels, width, width - 1, 0, [0, 255, 0, 255]);
-  drawPixel(pixels, width, width - 1, height - 1, [0, 0, 255, 255]);
-  drawPixel(pixels, width, 0, height - 1, [0, 0, 0, 255]);
+  var stems = plant.growth.stems;
+  var stemType = Math.max(plant.expression.traits.stem, 3);
+
+  // 0: branch every 2 pixels after 2 pixels
+  // 1: branch every 3 pixels after 3 pixels
+  // 2: branch every 4 pixels after 5 pixels
+  // 3: branch every 5 pixels after 8 pixels
+
+  // Paint first and second center pixel
+  var i = 0;
+  while (stems && i < height) {
+    drawPixel(pixels, width, width / 2, i, [255, 0, 0, 255]);
+    stems--;
+    i++;
+  }
   return pixels;
 }
