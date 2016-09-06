9e0d7fda97a609d6aeda6037dc0f097adbd12aed
Cindy Zhang <cixzhang@gmail.com>
2016-09-06 08:32:44 -0700

    naive growth nodes

--- src/create-image-data.js
+++ src/create-image-data.js
@@ -11,17 +11,37 @@ export default function createImageData(plant) {
 function generateStemPixels(plant, width, height) {
   var pixels = new Uint8ClampedArray(4 * width * height);
   var stems = plant.growth.stems;
+  var draw = (x, y) => drawPixel(pixels, width, x, y, [255, 0, 0, 255]);
   var stemType = Math.max(plant.expression.traits.stem, 3);
+  var growthNode = {
+    position: [width / 2, 0],
+    direction: [1, 0],
+    next: null
+  };
 
-  // 0: branch every 2 pixels after 2 pixels
-  // 1: branch every 3 pixels after 3 pixels
-  // 2: branch every 4 pixels after 5 pixels
-  // 3: branch every 5 pixels after 8 pixels
-
-  // Paint first and second center pixel
   var i = 0;
-  while (stems && i < height) {
-    drawPixel(pixels, width, width / 2, i, [255, 0, 0, 255]);
+  var x, y;
+  var currentNode = growthNode;
+  while (stems) {
+    x = currentNode.position[0];
+    y = currentNode.position[1];
+    draw(x, y);
+    currentNode.position[0] += currentNode.direction[0];
+    currentNode.position[1] += currentNode.direction[1];
+
+    if (stemType % 3 && i % (stemType % 3)) {
+      currentNode.next = {
+        position: [currentNode.position[0], currentNode.position[1]],
+        direction: [currentNode.direction[0], currentNode.direction[1] + 1],
+        next: {
+          position: [currentNode.position[0], currentNode.position[1]],
+          direction: [currentNode.direction[0], currentNode.direction[1] - 1],
+          next: null
+        }
+      }
+    }
+
+    currentNode = growthNode.next || growthNode;
     stems--;
     i++;
   }
