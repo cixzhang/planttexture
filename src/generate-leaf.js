0851d27d5690d2bd80c6eb04e6879cefcae2a497
Cindy Zhang <cixzhang@gmail.com>
2016-11-14 22:12:14 -0800

    growth leaves adjust number of leaves in sprite

--- src/generate-leaf.js
+++ src/generate-leaf.js
@@ -1,24 +1,26 @@
 import { drawPixel } from './helpers.js';
 
-var leaves = [
+var leafPixels = [
   [[1, 0], [2, 0], [2, -1], [3, -1]],
   [[-1, 0], [-2, 0], [-2, -1], [-3, -1]]
 ];
 
 export default function generateLeafPixels(plant, width, height, pixels, nodes) {
   var draw = (x, y, color) => drawPixel(pixels, width, x, y, color);
+  var leaves = plant.growth.leaves;
   var color = [0, 60, 50, 255];
 
-  nodes.forEach(function (node) {
+  nodes.forEach(function (node, nodeI) {
     var leaf = 0;
     color = [0, 50, 20, 255];
     node.growthNode.forEach(function (nodule, i) {
+      if (nodeI + i > leaves) return;
       if ((i + 1) % 2) return;
 
       leaf = (leaf + 1) % 2;
       var x = nodule[0];
       var y = nodule[1];
-      leaves[leaf].forEach(function (position) {
+      leafPixels[leaf].forEach(function (position) {
         draw(Math.floor(x + position[0]), Math.floor(y + position[1]), color);
       });
 
